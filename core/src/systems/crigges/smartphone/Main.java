83fba39f2dabbda87cc9215387962b6bd9913ff0
Crigges <andy400@web.de>
2016-07-20 00:23:09 +0200

    Some bt testing

--- core/src/systems/crigges/smartphone/Main.java
+++ core/src/systems/crigges/smartphone/Main.java
@@ -1,5 +1,8 @@
 package systems.crigges.smartphone;
 
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.LocalDevice;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,22 +14,23 @@ public class Main extends ApplicationAdapter {
 	Texture img;
 	
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
 	}
