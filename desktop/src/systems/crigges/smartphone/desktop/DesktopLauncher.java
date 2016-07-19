e5b997a47158734e762f1765d88e034c17aec61c
unknown <andy400@web.de>
2016-07-19 20:08:04 +0200

    init commit

---
+++ desktop/src/systems/crigges/smartphone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package systems.crigges.smartphone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import systems.crigges.smartphone.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
