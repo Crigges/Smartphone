154aed2ee77c8bab3abe55837f48614606af528c
unknown <andy400@web.de>
2016-07-22 11:03:09 +0200

    Client should work, but untested

--- android/src/systems/crigges/smartphone/ConnectTest.java
+++ android/src/systems/crigges/smartphone/ConnectTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
 
-import com.example.bttest.R;
+import com.badlogic.gdx.backends.android.AndroidApplication;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -19,12 +19,13 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 
-public class ConnectTest extends Activity {
-	TextView out;
+public class ConnectTest{
+	
 	private static final int REQUEST_ENABLE_BT = 1;
 	private BluetoothAdapter btAdapter = null;
 	private BluetoothSocket btSocket = null;
 	private OutputStream outStream = null;
+	private AndroidApplication app;
 
 	// Well known SPP UUID
 	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
@@ -32,32 +33,14 @@ public class ConnectTest extends Activity {
 	// Insert your server's MAC address
 	private static String address = "00:00:00:00:00:00";
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		out = (TextView) findViewById(R.id.out);
-
-		out.append("\n...In onCreate()...");
-
+	public ConnectTest(AndroidApplication app) {
+		this.app = app;
 		btAdapter = BluetoothAdapter.getDefaultAdapter();
-		CheckBTState();
+		checkBTState();
 	}
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		out.append("\n...In onStart()...");
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		out.append("\n...In onResume...\n...Attempting client connect...");
-
+	public void onResume(AndroidApplication app) {
+		
 		// Set up a pointer to the remote node using it's address.
 		for(BluetoothDevice d : btAdapter.getBondedDevices()){
 			System.out.println(d.getName());
@@ -71,7 +54,7 @@ public class ConnectTest extends Activity {
 		try {
 			btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
 		} catch (IOException e) {
-			AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+			alertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
 		}
 
 		// Discovery is resource intensive. Make sure it isn't going on
@@ -81,23 +64,21 @@ public class ConnectTest extends Activity {
 		// Establish the connection. This will block until it connects.
 		try {
 			btSocket.connect();
-			out.append("\n...Connection established and data link opened...");
 		} catch (IOException e) {
 			try {
 				btSocket.close();
 			} catch (IOException e2) {
-				AlertBox("Fatal Error",
+				alertBox("Fatal Error",
 						"In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
 			}
 		}
 
 		// Create a data stream so we can talk to server.
-		out.append("\n...Sending message to server...");
 
 		try {
 			outStream = btSocket.getOutputStream();
 		} catch (IOException e) {
-			AlertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+			alertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
 		}
 
 		String message = "Hello from Android.\n";
@@ -111,66 +92,33 @@ public class ConnectTest extends Activity {
 						+ ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
 			msg = msg + ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
 
-			AlertBox("Fatal Error", msg);
+			alertBox("Fatal Error", msg);
 		}
 	}
 
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		out.append("\n...In onPause()...");
-
-		if (outStream != null) {
-			try {
-				outStream.flush();
-			} catch (IOException e) {
-				AlertBox("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
-			}
-		}
-
-		try {
-			btSocket.close();
-		} catch (IOException e2) {
-			AlertBox("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-		}
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-		out.append("\n...In onStop()...");
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		out.append("\n...In onDestroy()...");
-	}
-
-	private void CheckBTState() {
+	private void checkBTState() {
 		// Check for Bluetooth support and then check to make sure it is turned
 		// on
 
 		// Emulator doesn't support Bluetooth and will return null
 		if (btAdapter == null) {
-			AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+			alertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
 		} else {
 			if (btAdapter.isEnabled()) {
-				out.append("\n...Bluetooth is enabled...");
+ 
 			} else {
 				// Prompt user to turn on Bluetooth
 				Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
-				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+				app.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 			}
 		}
 	}
 
-	public void AlertBox(String title, String message) {
-		new AlertDialog.Builder(this).setTitle(title).setMessage(message + " Press OK to exit.")
+	public void alertBox(String title, String message) {
+		new AlertDialog.Builder(app).setTitle(title).setMessage(message + " Press OK to exit.")
 				.setPositiveButton("OK", new OnClickListener() {
 					public void onClick(DialogInterface arg0, int arg1) {
-						finish();
+						app.finish();
 					}
 				}).show();
 	}
