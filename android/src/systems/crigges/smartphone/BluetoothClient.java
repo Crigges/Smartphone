74dae3e13191eb67cbdf1de55c0ab0b379766e95
Crigges <andy400@web.de>
2016-07-23 11:15:53 +0200

    Prototype Bluetooth Client is finsihed now | untested yet

--- android/src/systems/crigges/smartphone/BluetoothClient.java
+++ android/src/systems/crigges/smartphone/BluetoothClient.java
@@ -1,13 +1,17 @@
 package systems.crigges.smartphone;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.UUID;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.sun.crypto.provider.ARCFOURCipher;
-
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,12 +19,17 @@ import android.content.IntentFilter;
 
 public class BluetoothClient implements Client {
 	private static final int REQUEST_ENABLE_BT = 1;
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	
 	private AndroidApplication app;
 	private BluetoothAdapter btAdapter;
 	private ClientUI ui;
 	private ArrayList<BluetoothDevice> availableDevices;
 	private ArrayList<String> deviceAddresses;
+
+	private BluetoothSocket btSocket;
+	private OutputStream out;
+	private InputStream in;
 	
 	public BluetoothClient(AndroidApplication app, ClientUI ui){
 		this.app = app;
@@ -96,32 +105,103 @@ public class BluetoothClient implements Client {
 
 	@Override
 	public void stopClient() {
-		// TODO Auto-generated method stub
-		
+		if(btAdapter.isDiscovering()){
+			btAdapter.cancelDiscovery();
+		}
+		try {
+			if(btSocket != null){
+				out.close();
+				in.close();
+				btSocket.close();
+			}
+			ui.log("Stopped Client");
+		} catch (IOException e) {
+			ui.log("Failed to stop Client:\n" + e.getMessage());
+			ui.setStatus(Status.Ready);
+		}
 	}
 
 	@Override
 	public void connectToClient(String address) {
-		ui.setStatus(Status.Connecting);
-		if(btAdapter.isDiscovering()){
-			btAdapter.cancelDiscovery();
-		}
-		BluetoothDevice dev = btAdapter.getRemoteDevice(address);
-		ui.log("Connecting to" + dev.getName());
-		if(dev.getBondState() != BluetoothDevice.BOND_BONDED){
-			ui.log(dev.getName() + " is not bound yet, bounding...");
-			dev.createBond();
-			ui.log("Try again after device is bounded.");
-			return;
-		}else{
-			//TODO
-		}
+		Gdx.app.postRunnable(new ConnectionTask(address));
 	}
+	
+	public class ConnectionTask implements Runnable{
+		private String address;
+		
+		public ConnectionTask(String address) {
+			this.address = address;
+		}
+
+		@SuppressLint("NewApi")
+		@Override
+		public void run() {
+			ui.setStatus(Status.Connecting);
+			if(btAdapter.isDiscovering()){
+				btAdapter.cancelDiscovery();
+			}
+			BluetoothDevice dev = btAdapter.getRemoteDevice(address);
+			ui.log("Connecting to" + dev.getName());
+			if(dev.getBondState() != BluetoothDevice.BOND_BONDED){
+				ui.log(dev.getName() + " is not bound yet, bounding...");
+				dev.createBond();
+				ui.log("Try again after device is bounded.");
+				return;
+			}else{
+				try {
+					btSocket = dev.createRfcommSocketToServiceRecord(MY_UUID);
+					ui.log("Opened socket socket successfuly");
+				} catch (IOException e) {
+					ui.log("Opening socket failed:\n" + e.getMessage());
+					ui.setStatus(Status.Ready);
+					return;
+				}
+				try {
+					btSocket = dev.createRfcommSocketToServiceRecord(MY_UUID);
+					ui.log("Opened socket socket successfuly");
+				} catch (IOException e) {
+					ui.log("Opening socket failed:\n" + e.getMessage());
+					ui.setStatus(Status.Ready);
+					return;
+				}
+				try {
+					ui.log("Establish socket connection...");
+					btSocket.connect();
+				} catch (IOException e) {
+					ui.log("Socket connenction failed:\n" + e.getMessage());
+					ui.setStatus(Status.Ready);
+					return;
+				}
+				ui.log("Opening streams...");
+				try {
+					out = btSocket.getOutputStream();
+					in = btSocket.getInputStream();
+					ui.log("Connected successfuly. Audio data is beeing streamed now");
+					ui.setStatus(Status.Running);
+				} catch (IOException e) {
+					ui.log("Couldn't open streams:\n" + e.getMessage());
+					ui.setStatus(Status.Ready);
+				}
+			}
+		}
+		
+	}
+	
 
 	@Override
 	public void disconnect() {
-		// TODO Auto-generated method stub
-		
+		if(btAdapter.isDiscovering()){
+			btAdapter.cancelDiscovery();
+		}
+		try {
+			out.close();
+			in.close();
+			btSocket.close();
+			ui.log("Disconnected");
+		} catch (IOException e) {
+			ui.log("Failed to close connection:\n" + e.getMessage());
+			ui.setStatus(Status.Ready);
+		}
 	}
 
 }
